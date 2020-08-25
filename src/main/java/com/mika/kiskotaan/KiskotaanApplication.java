b04631eba1d5e377d50bad616cf27743a8595158
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-25 19:25:13 +0300

    Rename main class

--- src/main/java/com/mika/kiskotaan/KiskotaanApplication.java
+++ src/main/java/com/mika/kiskotaan/KiskotaanApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KiskotaanBackendApplication {
+public class KiskotaanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KiskotaanBackendApplication.class, args);
+		SpringApplication.run(KiskotaanApplication.class, args);
 	}
 
 }

de40f1cda643154dc7855df95ed29734f8c8b344
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-28 18:04:30 +0300

    Add more test data

--- src/test/java/com/mika/kiskotaan/testdata/TestModels.java
+++ src/test/java/com/mika/kiskotaan/testdata/TestModels.java
@@ -10,13 +10,18 @@ import java.util.stream.IntStream;
 public abstract class TestModels {
 
     public static List<Course> courses() {
-        Course c1 = new Course("Kuokkalan golf", holes(9), 30);
-        c1.setId(1L);
+        Course c1 = course();
 
         Course c2 = new Course("Laajavuoren golf", holes(18), 50);
         c2.setId(2L);
 
         return List.of(c1, c2);
+    }
+
+    public static Course course() {
+        Course course = new Course("Kuokkalan golf", holes(9), 30);
+        course.setId(1L);
+        return course;
     }
 
     public static List<Hole> holes(int numberOfHoles) {

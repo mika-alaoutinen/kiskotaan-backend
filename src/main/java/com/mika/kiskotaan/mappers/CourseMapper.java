5f7c6cb405fccb8e105284946c7b87c897072c42
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-31 19:25:07 +0300

    Edit mappers

--- src/main/java/com/mika/kiskotaan/mappers/CourseMapper.java
+++ src/main/java/com/mika/kiskotaan/mappers/CourseMapper.java
@@ -3,10 +3,14 @@ package com.mika.kiskotaan.mappers;
 import com.mika.kiskotaan.models.Course;
 import kiskotaan.openapi.model.CourseResource;
 import kiskotaan.openapi.model.NewCourseResource;
+import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED
+)
 @DecoratedWith(CourseMapperDecorator.class)
 public interface CourseMapper {
     Course toModel(NewCourseResource resource);
--- src/main/java/com/mika/kiskotaan/mappers/ScoreCardMapper.java
+++ src/main/java/com/mika/kiskotaan/mappers/ScoreCardMapper.java
@@ -3,11 +3,16 @@ package com.mika.kiskotaan.mappers;
 import com.mika.kiskotaan.models.ScoreCard;
 import kiskotaan.openapi.model.NewScoreCardResource;
 import kiskotaan.openapi.model.ScoreCardResource;
+import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring", uses = { CourseMapper.class, PlayerMapper.class, ScoreMapper.class })
+@Mapper(
+        componentModel = "spring",
+        uses = { CourseMapper.class, PlayerMapper.class, ScoreMapper.class },
+        collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED
+)
 @DecoratedWith(ScoreCardMapperDecorator.class)
 public interface ScoreCardMapper {
     ScoreCard toModel(ScoreCardResource resource);

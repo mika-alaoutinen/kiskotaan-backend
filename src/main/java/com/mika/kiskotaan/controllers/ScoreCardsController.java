31e843de4c99ed7a16a0a8bf9377e6310e35c1aa
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-31 16:55:31 +0300

    Implement controller

--- src/main/java/com/mika/kiskotaan/controllers/ScoreCardsController.java
+++ src/main/java/com/mika/kiskotaan/controllers/ScoreCardsController.java
@@ -1,6 +1,7 @@
 package com.mika.kiskotaan.controllers;
 
 import com.mika.kiskotaan.services.ScoreCardService;
+import com.mika.kiskotaan.services.ScoreService;
 import kiskotaan.openapi.api.ScoreCardsApi;
 import kiskotaan.openapi.model.NewScoreCardResource;
 import kiskotaan.openapi.model.ScoreCardResource;
@@ -16,26 +17,27 @@ import java.math.BigDecimal;
 @RestController
 @RequiredArgsConstructor
 public class ScoreCardsController implements ScoreCardsApi {
-    private final ScoreCardService service;
+    private final ScoreService scoreService;
+    private final ScoreCardService scoreCardService;
 
     @Override
     public ResponseEntity<ScoreCardResource> addScoreCard(@Valid NewScoreCardResource newScoreCardResource) {
-        return new ResponseEntity<>(service.addScoreCard(newScoreCardResource), HttpStatus.CREATED);
+        return new ResponseEntity<>(scoreCardService.addScoreCard(newScoreCardResource), HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<Void> deleteScoreCard(BigDecimal id) {
-        service.deleteScoreCard(id.longValue());
+        scoreCardService.deleteScoreCard(id.longValue());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @Override
     public ResponseEntity<ScoreCardResource> getScoreCard(BigDecimal id) {
-        return ResponseEntity.ok(service.getScoreCard(id.longValue()));
+        return ResponseEntity.ok(scoreCardService.getScoreCard(id.longValue()));
     }
 
     @Override
     public ResponseEntity<ScoreRowResource> updateScores(BigDecimal id, @Valid ScoreRowResource scoreRowResource) {
-        return null;
+        return ResponseEntity.ok(scoreService.editScoreRow(id.longValue(), scoreRowResource));
     }
 }

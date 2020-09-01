96a045a7b2c647f0453233f59497d2b905e00e86
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-09-01 21:55:37 +0300

    Refactor decorator

--- src/main/java/com/mika/kiskotaan/mappers/ScoreCardMapperDecorator.java
+++ src/main/java/com/mika/kiskotaan/mappers/ScoreCardMapperDecorator.java
@@ -2,11 +2,9 @@ package com.mika.kiskotaan.mappers;
 
 import com.mika.kiskotaan.models.*;
 import kiskotaan.openapi.model.NewScoreCardResource;
-import kiskotaan.openapi.model.ScoreCardResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,24 +17,24 @@ public abstract class ScoreCardMapperDecorator implements ScoreCardMapper {
     @Override
     public ScoreCard toModel(NewScoreCardResource resource) {
         ScoreCard scoreCard = mapper.toModel(resource);
-        var rows = createScoreRows(scoreCard);
-        scoreCard.setRows(rows);
+        scoreCard.setRows(createScoreRows(scoreCard));
         return scoreCard;
     }
 
     private List<ScoreRow> createScoreRows(ScoreCard scoreCard) {
-        List<ScoreRow> rows = new ArrayList<>();
+        return scoreCard.getCourse().getHoles().stream()
+                .map(hole -> mapScoreRow(hole, scoreCard.getPlayers()))
+                .collect(Collectors.toList());
+    }
 
-        for (Hole hole : scoreCard.getCourse().getHoles()) {
-            ScoreRow row = new ScoreRow();
-            row.setHole(hole.getNumber());
+    private ScoreRow mapScoreRow(Hole hole, List<Player> players) {
+        ScoreRow row = new ScoreRow();
 
-            var scores = mapScores(scoreCard.getPlayers(), hole.getPar());
-            row.setScores(scores);
-            rows.add(row);
-        }
+        var scores = mapScores(players, hole.getPar());
+        row.setScores(scores);
+        row.setHole(hole.getNumber());
 
-        return rows;
+        return row;
     }
 
     private List<Score> mapScores(List<Player> players, int par) {

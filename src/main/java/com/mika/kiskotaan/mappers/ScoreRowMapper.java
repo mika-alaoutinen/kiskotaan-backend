065bb094d033d1cccd8d08850663b4e4023ce33e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-09-02 20:39:03 +0300

    Change ScoreRowMapper mapping strategy to fix test failure

--- src/main/java/com/mika/kiskotaan/mappers/ScoreRowMapper.java
+++ src/main/java/com/mika/kiskotaan/mappers/ScoreRowMapper.java
@@ -2,10 +2,15 @@ package com.mika.kiskotaan.mappers;
 
 import com.mika.kiskotaan.models.ScoreRow;
 import kiskotaan.openapi.model.ScoreRowResource;
+import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring", uses = ScoreMapper.class)
+@Mapper(
+        componentModel = "spring",
+        collectionMappingStrategy = CollectionMappingStrategy.TARGET_IMMUTABLE, // Fixes a controller test case failure
+        uses = ScoreMapper.class
+)
 public interface ScoreRowMapper {
     ScoreRow toModel(ScoreRowResource resource);
     ScoreRowResource toResources(ScoreRow model);

21111f7b9fd231871daff8f6e844e7f3a8bf5318
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-09-05 11:50:44 +0300

    WIP validator for new score card

---
+++ src/main/java/com/mika/kiskotaan/validators/NewResourceValidator.java
@@ -0,0 +1,41 @@
+package com.mika.kiskotaan.validators;
+
+import com.mika.kiskotaan.errors.badrequest.BadRequestException;
+import com.mika.kiskotaan.services.CourseService;
+import com.mika.kiskotaan.services.PlayerService;
+import kiskotaan.openapi.model.CourseResource;
+import kiskotaan.openapi.model.NewScoreCardResource;
+import kiskotaan.openapi.model.PlayerResource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NewResourceValidator {
+    private final CourseService courseService;
+    private final PlayerService playerService;
+
+    public NewScoreCardResource validateNewResource(NewScoreCardResource resource) throws BadRequestException {
+        validateCourseExists(resource.getCourse());
+        validatePlayersExist(resource.getPlayers());
+        return resource;
+    }
+
+    private void validateCourseExists(CourseResource courseResource) {
+        if (!courseService.existsById(courseResource.getId().longValue())) {
+            throw new BadRequestException(courseResource);
+        }
+    }
+
+    private void validatePlayersExist(List<PlayerResource> playerResources) {
+        boolean allPlayersExistInRepository = playerResources.stream()
+                .mapToLong(player -> player.getId().longValue())
+                .allMatch(playerService::existsById);
+
+        if (!allPlayersExistInRepository) {
+            throw new BadRequestException(new PlayerResource());
+        }
+    }
+}

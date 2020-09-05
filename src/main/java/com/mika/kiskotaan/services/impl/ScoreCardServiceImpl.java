0e21e7c45b606eb01f1fe23de6a2723dc0a29be1
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-09-05 16:13:45 +0300

    Update ScoreCardServiceImpl

--- src/main/java/com/mika/kiskotaan/services/impl/ScoreCardServiceImpl.java
+++ src/main/java/com/mika/kiskotaan/services/impl/ScoreCardServiceImpl.java
@@ -3,8 +3,12 @@ package com.mika.kiskotaan.services.impl;
 import com.mika.kiskotaan.errors.badrequest.ScoreCardException;
 import com.mika.kiskotaan.errors.notfound.NotFoundException;
 import com.mika.kiskotaan.mappers.ScoreCardMapper;
+import com.mika.kiskotaan.models.Course;
+import com.mika.kiskotaan.models.Player;
 import com.mika.kiskotaan.models.ScoreCard;
 import com.mika.kiskotaan.repositories.ScoreCardRepository;
+import com.mika.kiskotaan.services.CourseService;
+import com.mika.kiskotaan.services.PlayerService;
 import com.mika.kiskotaan.services.ScoreCardService;
 import com.mika.kiskotaan.validators.ScoreCardResourceValidator;
 import kiskotaan.openapi.model.NewScoreCardResource;
@@ -12,7 +16,7 @@ import kiskotaan.openapi.model.ScoreCardResource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.stream.Stream;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +24,9 @@ public class ScoreCardServiceImpl implements ScoreCardService {
     private final ScoreCardMapper mapper;
     private final ScoreCardRepository repository;
     private final ScoreCardResourceValidator validator;
+
+    private final CourseService courseService;
+    private final PlayerService playerService;
 
     @Override
     public ScoreCardResource getScoreCard(Long id) throws NotFoundException {
@@ -30,12 +37,14 @@ public class ScoreCardServiceImpl implements ScoreCardService {
 
     @Override
     public ScoreCardResource addScoreCard(final NewScoreCardResource resource) throws ScoreCardException {
-        return Stream.of(validator.validateNewResource(resource))
-                .map(mapper::toModel)
-                .map(repository::save)
-                .map(mapper::toResource)
-                .findAny()
-                .orElseThrow(() -> new ScoreCardException(resource));
+        NewScoreCardResource validated = validator.validateNewResource(resource);
+
+        Course course = courseService.getCourse(validated.getCourseId().longValue());
+        List<Player> players = playerService.getPlayers(validated.getPlayersIds());
+        ScoreCard scoreCard = mapper.toScoreCard(course, players);
+
+        ScoreCard saved = repository.save(scoreCard);
+        return mapper.toResource(saved);
     }
 
     @Override

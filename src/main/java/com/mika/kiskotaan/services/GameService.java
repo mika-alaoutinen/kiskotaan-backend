8e3b9666c8375ac1bf4287345f16c2f1991870b4
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-09-06 19:20:05 +0300

    Add stub dao for game operations

--- src/main/java/com/mika/kiskotaan/services/GameService.java
+++ src/main/java/com/mika/kiskotaan/services/GameService.java
@@ -1,4 +1,9 @@
 package com.mika.kiskotaan.services;
 
+import com.mika.kiskotaan.errors.notfound.NotFoundException;
+import kiskotaan.openapi.model.GameResource;
+
 public interface GameService {
+    GameResource startGame(Long scoreCardId);
+    GameResource endGame(Long scoreCardId) throws NotFoundException;
 }
--- src/main/java/com/mika/kiskotaan/services/impl/GameServiceImpl.java
+++ src/main/java/com/mika/kiskotaan/services/impl/GameServiceImpl.java
@@ -1,12 +1,24 @@
 package com.mika.kiskotaan.services.impl;
 
-import com.mika.kiskotaan.repositories.GameRepository;
+import com.mika.kiskotaan.dao.GameDao;
+import com.mika.kiskotaan.errors.notfound.NotFoundException;
 import com.mika.kiskotaan.services.GameService;
+import kiskotaan.openapi.model.GameResource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class GameServiceImpl implements GameService {
-    private final GameRepository repository;
+    private final GameDao dao;
+
+    @Override
+    public GameResource startGame(Long scoreCardId) {
+        return null;
+    }
+
+    @Override
+    public GameResource endGame(Long scoreCardId) throws NotFoundException {
+        return null;
+    }
 }

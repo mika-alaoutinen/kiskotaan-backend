21b2761f9385bfcdaee229cb17f216818d9d09c8
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-11-09 16:58:52 +0200

    Init project

---
+++ src/main/java/mikaa/players/PlayersApplication.java
@@ -0,0 +1,13 @@
+package mikaa.players;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersApplication.class, args);
+	}
+
+}

b0fa75b1fa7208ac8dee190c62981fa7aaf2c69d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-11-09 17:37:29 +0200

    Change application port

--- src/main/java/mikaa/players/PlayersApplication.java
+++ src/main/java/mikaa/players/PlayersApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlayersApplication {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		SpringApplication.run(PlayersApplication.class, args);
 	}
 

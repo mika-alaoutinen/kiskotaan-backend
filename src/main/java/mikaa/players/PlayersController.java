958fe4b96246c1932169fee4419699df5b32d0a8
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2022-11-11 18:16:36 +0200

    Openapi codegen (#2)
    Setup code generation from an OpenAPI document

--- src/main/java/mikaa/players/PlayersController.java
+++ src/main/java/mikaa/players/PlayersController.java
@@ -1,14 +1,41 @@
 package mikaa.players;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-class PlayersController {
+import jakarta.validation.Valid;
+import mikaa.api.PlayersApi;
+import mikaa.model.NewPlayerDTO;
+import mikaa.model.PlayerDTO;
 
-  @GetMapping("hello")
-  ResponseEntity<String> hello() {
-    return ResponseEntity.ok("Hello");
+@RestController
+class PlayersController implements PlayersApi {
+
+  @Override
+  public ResponseEntity<PlayerDTO> addPlayer(@Valid NewPlayerDTO newPlayer) {
+    throw new UnsupportedOperationException("TODO");
   }
+
+  @Override
+  public ResponseEntity<Void> deletePlayer(Integer id) {
+    throw new UnsupportedOperationException("TODO");
+  }
+
+  @Override
+  public ResponseEntity<PlayerDTO> getPlayer(Integer id) {
+    throw new UnsupportedOperationException("TODO");
+  }
+
+  @Override
+  public ResponseEntity<List<PlayerDTO>> getPlayers() {
+    throw new UnsupportedOperationException("TODO");
+  }
+
+  @Override
+  public ResponseEntity<PlayerDTO> updatePlayer(Integer id, @Valid NewPlayerDTO newPlayer) {
+    throw new UnsupportedOperationException("TODO");
+  }
+
 }

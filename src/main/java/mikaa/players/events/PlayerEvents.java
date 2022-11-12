1c10b7b9b71449ffb50b683d837c8786bdd0de42
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-11-12 18:18:00 +0200

    Add trusted packages

---
+++ src/main/java/mikaa/players/events/PlayerEvents.java
@@ -0,0 +1,20 @@
+package mikaa.players.events;
+
+public interface PlayerEvents {
+
+  public record PlayerEvent(EventType type, Player player) {
+  }
+
+  enum EventType {
+    PLAYER_ADDED, PLAYER_UPDATED
+  }
+
+  public static PlayerEvent add(Player player) {
+    return new PlayerEvent(EventType.PLAYER_ADDED, player);
+  }
+
+  public static PlayerEvent update(Player player) {
+    return new PlayerEvent(EventType.PLAYER_UPDATED, player);
+  }
+
+}

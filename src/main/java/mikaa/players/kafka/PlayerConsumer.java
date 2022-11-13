372892fc9c00ceecf0e3ab8cd48ba12d5c0f8945
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-11-13 19:44:34 +0200

    Change consumer to info log events

--- src/main/java/mikaa/players/kafka/PlayerConsumer.java
+++ src/main/java/mikaa/players/kafka/PlayerConsumer.java
@@ -3,14 +3,16 @@ package mikaa.players.kafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
 import mikaa.players.events.PlayerEvents.PlayerEvent;
 
 @Component
+@Slf4j
 class PlayerConsumer {
 
   @KafkaListener(topics = KafkaTopic.PLAYERS, groupId = "players-group")
   void consume(PlayerEvent event) {
-    System.out.println("consume " + event);
+    log.info("consumed event " + event);
   }
 
 }

cfb880a84b9aa9de33513bf3a651603b999152a3
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-11-17 17:12:07 +0200

    Move logic inside record classes

--- courses-service/src/main/java/mikaa/course/CourseService.java
+++ courses-service/src/main/java/mikaa/course/CourseService.java
@@ -7,26 +7,18 @@ import javax.enterprise.context.ApplicationScoped;
 
 import mikaa.dto.CourseDTO;
 import mikaa.dto.CourseSummaryDTO;
-import mikaa.dto.HoleDTO;
 
 @ApplicationScoped
 class CourseService {
 
   List<CourseSummaryDTO> findAll() {
     List<CourseEntity> entities = CourseEntity.listAll();
-    return entities.stream().map(CourseService::toSummary).toList();
+    return entities.stream().map(CourseSummaryDTO::new).toList();
   }
 
   Optional<CourseDTO> findOne(long id) {
     Optional<CourseEntity> courseOpt = CourseEntity.findByIdOptional(id);
-    return courseOpt.map(course -> {
-      var holes = course.holes.stream().map(HoleDTO::new).toList();
-      return new CourseDTO(course.id, course.name, holes);
-    });
+    return courseOpt.map(CourseDTO::new);
   }
 
-  private static CourseSummaryDTO toSummary(CourseEntity entity) {
-    var coursePar = entity.holes.stream().mapToInt(h -> h.par).sum();
-    return new CourseSummaryDTO(entity.id, entity.name, entity.holes.size(), coursePar);
-  }
 }
--- courses-service/src/main/java/mikaa/dto/CourseDTO.java
+++ courses-service/src/main/java/mikaa/dto/CourseDTO.java
@@ -3,7 +3,16 @@ package mikaa.dto;
 import java.util.List;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
+import mikaa.course.CourseEntity;
 
 @RegisterForReflection
 public record CourseDTO(Long id, String name, List<HoleDTO> hole) {
+
+  public CourseDTO(CourseEntity entity) {
+    this(entity.id, entity.name, holes(entity));
+  }
+
+  private static List<HoleDTO> holes(CourseEntity entity) {
+    return entity.holes.stream().map(HoleDTO::new).toList();
+  }
 }

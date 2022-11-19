4de462c958819230d2f62b9c6b7364b8f6d39358
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-11-19 20:44:08 +0200

    Update DTOs

--- courses-service/src/main/java/mikaa/dto/CourseDTO.java
+++ courses-service/src/main/java/mikaa/dto/CourseDTO.java
@@ -3,16 +3,7 @@ package mikaa.dto;
 import java.util.List;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import mikaa.feature.CourseEntity;
 
 @RegisterForReflection
 public record CourseDTO(long id, String name, List<HoleDTO> holes) {
-
-  public CourseDTO(CourseEntity entity) {
-    this(entity.getId(), entity.getName(), holes(entity));
-  }
-
-  private static List<HoleDTO> holes(CourseEntity entity) {
-    return entity.getHoles().stream().map(HoleDTO::new).toList();
-  }
 }
--- courses-service/src/main/java/mikaa/dto/CourseSummaryDTO.java
+++ courses-service/src/main/java/mikaa/dto/CourseSummaryDTO.java
@@ -1,24 +1,7 @@
 package mikaa.dto;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import mikaa.feature.CourseEntity;
 
 @RegisterForReflection
 public record CourseSummaryDTO(long id, String name, int holes, int par) {
-
-  public CourseSummaryDTO(CourseEntity entity) {
-    this(
-        entity.getId(),
-        entity.getName(),
-        courseHoleCount(entity),
-        coursePar(entity));
-  }
-
-  private static int courseHoleCount(CourseEntity entity) {
-    return entity.getHoles().size();
-  }
-
-  private static int coursePar(CourseEntity entity) {
-    return entity.getHoles().stream().mapToInt(h -> h.par).sum();
-  }
 }

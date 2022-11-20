3986701235ef11549a08a0008ed360113e2d2e2d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-11-20 14:37:31 +0200

    Return RestResponses from GlobalErrorHandler

--- courses-service/src/main/java/mikaa/infra/GlobalErrorHandler.java
+++ courses-service/src/main/java/mikaa/infra/GlobalErrorHandler.java
@@ -1,15 +1,12 @@
 package mikaa.infra;
 
-import java.util.List;
-
 import javax.enterprise.context.RequestScoped;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
 
+import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -27,28 +24,21 @@ class GlobalErrorHandler {
   private final UriInfo uri;
 
   @ServerExceptionMapper(ConstraintViolationException.class)
-  Response handleConstraintViolation(ConstraintViolationException ex) {
+  RestResponse<ValidationErrorBody> handleConstraintViolation(ConstraintViolationException ex) {
+    var body = fromException(ex, uri);
     log.info(ex.getMessage());
 
-    return Response
-        .status(Status.BAD_REQUEST)
-        .type(MediaType.APPLICATION_JSON)
-        .entity(fromException(ex, uri))
-        .build();
+    return RestResponse.status(Status.BAD_REQUEST, body);
   }
 
   @ServerExceptionMapper(NotFoundException.class)
-  Response handleNotFound(NotFoundException ex) {
+  RestResponse<ErrorBody> handleNotFound(NotFoundException ex) {
     String msg = ex.getMessage();
     String path = getPath(uri);
+    var body = new ErrorBody(404, "Not Found", msg, path);
 
     log.info(msg);
-
-    return Response
-        .status(Status.NOT_FOUND)
-        .type(MediaType.APPLICATION_JSON)
-        .entity(new ErrorBody(404, "Not Found", msg, path))
-        .build();
+    return RestResponse.status(Status.NOT_FOUND, body);
   }
 
   private static ValidationErrorBody fromException(ConstraintViolationException ex, UriInfo uri) {

26003a8d81b6371fe5de28212850291f48ed054b
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-12-04 18:21:48 +0200

    Remove courseId from HoleDTO

--- courses-service/src/main/java/mikaa/feature/HoleMapper.java
+++ courses-service/src/main/java/mikaa/feature/HoleMapper.java
@@ -6,12 +6,7 @@ import mikaa.dto.NewHoleDTO;
 interface HoleMapper {
 
   static HoleDTO dto(HoleEntity entity) {
-    return new HoleDTO(
-        entity.getId(),
-        entity.getCourse().getId(),
-        entity.getHoleNumber(),
-        entity.getPar(),
-        entity.getDistance());
+    return new HoleDTO(entity.getId(), entity.getHoleNumber(), entity.getPar(), entity.getDistance());
   }
 
   static HoleEntity entity(NewHoleDTO newHole) {

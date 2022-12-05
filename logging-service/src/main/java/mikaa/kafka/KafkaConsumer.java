846d4f7f79e0eeeb29862ad55885d01065ff5e71
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-12-05 13:06:17 +0200

    WIP KafkaConsumer

---
+++ logging-service/src/main/java/mikaa/kafka/KafkaConsumer.java
@@ -0,0 +1,24 @@
+package mikaa.kafka;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+class KafkaConsumer {
+
+  private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+  @Incoming("courses-in")
+  void consumeCourses(String event) {
+    log.info(event);
+  }
+
+  @Incoming("players-in")
+  void consumePlayers(String event) {
+    log.info(event);
+  }
+
+}

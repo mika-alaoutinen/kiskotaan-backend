096862d13ee5f865a58e1ef4d9ff460e63e9020c
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-12-05 12:29:57 +0200

    Add tests for liveness checks

--- logging-service/src/main/java/mikaa/infra/LivenessCheck.java
+++ logging-service/src/main/java/mikaa/infra/LivenessCheck.java
@@ -1,10 +1,13 @@
 package mikaa.infra;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Liveness;
 
 @Liveness
+@ApplicationScoped
 class LivenessCheck implements HealthCheck {
 
     @Override

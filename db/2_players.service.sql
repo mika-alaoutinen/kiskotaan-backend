9681ed061719a92dba000ec8a8dbdc8b9e56829a
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-12-10 19:59:40 +0200

    Add players service to compose file

---
+++ db/2_players.service.sql
@@ -0,0 +1,8 @@
+\c testdb;
+
+-- Create a new table
+CREATE TABLE IF NOT EXISTS player (
+    id          SERIAL PRIMARY KEY,
+    first_name   varchar(50) NOT NULL,
+    last_name    varchar(50) NOT NULL
+);

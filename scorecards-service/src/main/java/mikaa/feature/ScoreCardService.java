b827cbaecb8aa7b0243a0a436da51da104596a20
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2022-12-30 16:42:18 +0200

    Add scorecard (#35)
    Implement adding new score card

--- scorecards-service/src/main/java/mikaa/feature/ScoreCardService.java
+++ scorecards-service/src/main/java/mikaa/feature/ScoreCardService.java
@@ -1,16 +1,19 @@
 package mikaa.feature;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 
 import lombok.RequiredArgsConstructor;
+import mikaa.model.NewScoreCardDTO;
 
 @ApplicationScoped
 @RequiredArgsConstructor
 class ScoreCardService {
 
+  private final CourseService courseService;
   private final ScoreCardRepository repository;
 
   List<ScoreCardEntity> findAll() {
@@ -19,6 +22,19 @@ class ScoreCardService {
 
   Optional<ScoreCardEntity> findOne(long id) {
     return repository.findByIdOptional(id);
+  }
+
+  ScoreCardEntity add(NewScoreCardDTO newScoreCard) {
+    var course = courseService.findOrThrow(newScoreCard.getCourseId());
+
+    var playerIds = newScoreCard.getPlayersIds()
+        .stream()
+        .map(BigDecimal::longValue)
+        .toList();
+
+    ScoreCardEntity entity = new ScoreCardEntity(course, playerIds);
+    repository.persist(entity);
+    return entity;
   }
 
 }
--- scorecards-service/src/test/java/mikaa/feature/ScoreCardResourceTest.java
+++ scorecards-service/src/test/java/mikaa/feature/ScoreCardResourceTest.java
@@ -6,17 +6,21 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
+import mikaa.model.NewScoreCardDTO;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @QuarkusTest
 class ScoreCardResourceTest {
@@ -25,6 +29,9 @@ class ScoreCardResourceTest {
 
   @InjectMock
   private ScoreCardRepository repository;
+
+  @InjectMock
+  private CourseRepository courseRepository;
 
   @Test
   void should_get_all_score_cards() {
@@ -62,13 +69,59 @@ class ScoreCardResourceTest {
   }
 
   @Test
-  void get_course_returns_404() {
+  void get_score_card_returns_404() {
     var response = given()
         .when()
         .get(ENDPOINT + "/1")
         .then();
 
     assertNotFoundResponse(response, 1);
+  }
+
+  @Test
+  void should_add_new_score_card() {
+    when(courseRepository.findByIdOptional(anyLong())).thenReturn(Optional.of(new CourseEntity(1L, 18, null)));
+    
+    var newScoreCard = new NewScoreCardDTO()
+        .courseId(BigDecimal.valueOf(1))
+        .playersIds(Set.of(BigDecimal.valueOf(2), BigDecimal.valueOf(3)));
+
+    given()
+        .contentType(ContentType.JSON)
+        .body(newScoreCard)
+        .when()
+        .post(ENDPOINT)
+        .then()
+        .statusCode(200) // see readme for problem description
+        .contentType(ContentType.JSON)
+        .body(
+            "course.id", is(1),
+            "course.holes", is(18),
+            "playerIds", hasItems(2, 3),
+            "scores", empty());
+
+    verify(repository, atLeastOnce()).persist(any(ScoreCardEntity.class));
+  }
+
+  @Test
+  void post_score_card_should_throw_404_when_course_not_found() {
+    when(courseRepository.findByIdOptional(anyLong())).thenReturn(Optional.empty());
+
+    var newScoreCard = new NewScoreCardDTO()
+        .courseId(BigDecimal.valueOf(1))
+        .playersIds(Set.of(BigDecimal.valueOf(2), BigDecimal.valueOf(3)));
+
+    given()
+        .contentType(ContentType.JSON)
+        .body(newScoreCard)
+        .when()
+        .post(ENDPOINT)
+        .then()
+        .statusCode(404)
+        .contentType(ContentType.JSON)
+        .body("message", is("Could not find course with id 1"));
+    
+    verify(repository, never()).persist(any(ScoreCardEntity.class));
   }
 
   private static void assertNotFoundResponse(ValidatableResponse response, int id) {

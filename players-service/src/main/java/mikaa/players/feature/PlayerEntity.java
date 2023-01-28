7174650bda26da36c7d02daceb8b473a92faf2c6
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-01-28 10:21:07 +0200

    Organize imports

--- players-service/src/main/java/mikaa/players/feature/PlayerEntity.java
+++ players-service/src/main/java/mikaa/players/feature/PlayerEntity.java
@@ -2,13 +2,13 @@ package mikaa.players.feature;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;

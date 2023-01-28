c6120cad272e7f636a47553b7833a678c8638ff5
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-01-28 18:38:46 +0200

    Fix constructor

--- scorecards-service/src/main/java/mikaa/feature/player/PlayerEntity.java
+++ scorecards-service/src/main/java/mikaa/feature/player/PlayerEntity.java
@@ -50,6 +50,7 @@ public class PlayerEntity {
   }
 
   public PlayerEntity(Long id, String firstName, String lastName) {
+    this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
   }

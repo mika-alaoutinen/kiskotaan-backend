24d60dfedaa275587196657ee2bb311fb6562264
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-01-28 10:50:12 +0200

    Add tests for course events handling

--- scorecards-service/src/main/java/mikaa/feature/course/CourseConsumer.java
+++ scorecards-service/src/main/java/mikaa/feature/course/CourseConsumer.java
@@ -6,17 +6,35 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import lombok.RequiredArgsConstructor;
 import mikaa.events.course.CourseEvent;
 
 @ApplicationScoped
+@RequiredArgsConstructor
 class CourseConsumer {
 
   private static final Logger log = LoggerFactory.getLogger(CourseConsumer.class);
+  private final CourseService service;
 
   @Incoming("courses-in")
   void consume(CourseEvent event) {
-    log.info("type " + event.type());
-    log.info("payload " + event.payload());
+    var type = event.type();
+    var payload = event.payload();
+
+    log.info("type " + type);
+    log.info("payload " + payload);
+
+    switch (type) {
+      case COURSE_ADDED:
+        break;
+      case COURSE_DELETED:
+        break;
+      case COURSE_UPDATED:
+        break;
+      default:
+        log.warn("Unrecognized event type " + type);
+        break;
+    }
   }
 
 }
---
+++ scorecards-service/src/test/java/mikaa/feature/course/CourseEventsTest.java
@@ -0,0 +1,76 @@
+package mikaa.feature.course;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.smallrye.reactive.messaging.providers.connectors.InMemoryConnector;
+import io.smallrye.reactive.messaging.providers.connectors.InMemorySource;
+import mikaa.events.course.CourseEvent;
+import mikaa.events.course.CourseEventType;
+import mikaa.events.course.CoursePayload;
+
+@QuarkusTest
+class CourseEventsTest {
+
+  private static final CoursePayload LAAJIS = new CoursePayload(111, "Laajis", List.of());
+
+  @Any
+  @Inject
+  private InMemoryConnector connector;
+
+  @InjectMock
+  private CourseRepository repository;
+
+  private InMemorySource<CourseEvent> source;
+
+  @BeforeEach
+  void setup() {
+    source = connector.source("courses-in");
+  }
+
+  @Test
+  void should_save_new_course() {
+    source.send(new CourseEvent(CourseEventType.COURSE_ADDED, LAAJIS));
+    verify(repository, atLeastOnce()).persist(any(CourseEntity.class));
+  }
+
+  @Test
+  void should_update_course() {
+    var course = new CourseEntity(222L, 18, "Laajis", Set.of());
+    when(repository.findByIdOptional(anyLong())).thenReturn(Optional.of(course));
+
+    source.send(new CourseEvent(CourseEventType.COURSE_UPDATED, LAAJIS));
+    verify(repository, atLeastOnce()).persist(any(CourseEntity.class));
+  }
+
+  @Test
+  void should_do_nothing_on_update_if_course_not_found() {
+    when(repository.findByIdOptional(anyLong())).thenReturn(Optional.empty());
+
+    source.send(new CourseEvent(CourseEventType.COURSE_UPDATED, LAAJIS));
+    verify(repository, never()).persist(any(CourseEntity.class));
+  }
+
+  @Test
+  void should_delete_course() {
+    source.send(new CourseEvent(CourseEventType.COURSE_UPDATED, LAAJIS));
+    verify(repository, atLeastOnce()).deleteById(111L);
+  }
+
+}

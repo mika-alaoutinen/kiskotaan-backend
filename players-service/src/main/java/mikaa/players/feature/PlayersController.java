370b2eac7eb2fe4f12872422ba322806e5bea16d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-01-28 10:18:20 +0200

    Update player service to Spring Boot 3

--- players-service/src/main/java/mikaa/players/feature/PlayersController.java
+++ players-service/src/main/java/mikaa/players/feature/PlayersController.java
@@ -2,7 +2,7 @@ package mikaa.players.feature;
 
 import java.util.List;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;

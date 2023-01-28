2325e2b47b6e6b0064c0d5db3dce48a2d08a2fd7
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-01-28 20:59:38 +0200

    Replace course consumer test with service test

--- scorecards-service/src/test/java/mikaa/feature/course/CourseServiceTest.java
+++ scorecards-service/src/test/java/mikaa/feature/course/CourseServiceTest.java
@@ -11,38 +11,27 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import javax.enterprise.inject.Any;
-import javax.inject.Inject;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
-import io.smallrye.reactive.messaging.providers.connectors.InMemoryConnector;
-import io.smallrye.reactive.messaging.providers.connectors.InMemorySource;
-import mikaa.events.course.CourseEvent;
-import mikaa.events.course.CourseEventType;
 import mikaa.events.course.CoursePayload;
 import mikaa.events.course.Hole;
 
 @QuarkusTest
-class CourseEventsTest {
+class CourseServiceTest {
 
-  static final CoursePayload LAAJIS = new CoursePayload(111, "Laajis", List.of());
-
-  @Any
-  @Inject
-  InMemoryConnector connector;
+  static final CoursePayload LAAJIS = new CoursePayload(111L, "Laajis", List.of());
 
   @InjectMock
   CourseRepository repository;
 
-  InMemorySource<CourseEvent> source;
+  CourseService service;
 
   @BeforeEach
   void setup() {
-    source = connector.source("courses-in");
+    service = new CourseService(repository);
   }
 
   @Test
@@ -50,31 +39,28 @@ class CourseEventsTest {
     var hole = new Hole(222, 1, 3, 85);
     var newCourse = new CoursePayload(111L, "Laajis", List.of(hole));
 
-    source.send(new CourseEvent(CourseEventType.COURSE_ADDED, newCourse));
-    var persistedEntity = new CourseEntity(null, 1, "Laajis", Set.of());
-    verify(repository, atLeastOnce()).persist(persistedEntity);
+    service.add(newCourse);
+    verify(repository, atLeastOnce()).persist(new CourseEntity(null, 1, "Laajis", Set.of()));
   }
 
   @Test
   void should_update_course() {
-    var course = new CourseEntity(222L, 18, "Laajis", Set.of());
+    var course = new CourseEntity(111L, 24, "Kaihu", Set.of());
     when(repository.findByIdOptional(anyLong())).thenReturn(Optional.of(course));
 
-    source.send(new CourseEvent(CourseEventType.COURSE_UPDATED, LAAJIS));
-    verify(repository, atLeastOnce()).persist(any(CourseEntity.class));
+    service.update(LAAJIS);
+    verify(repository, atLeastOnce()).persist(new CourseEntity(111L, 24, "Laajis", Set.of()));
   }
 
   @Test
   void should_do_nothing_on_update_if_course_not_found() {
-    when(repository.findByIdOptional(anyLong())).thenReturn(Optional.empty());
-
-    source.send(new CourseEvent(CourseEventType.COURSE_UPDATED, LAAJIS));
+    service.update(LAAJIS);
     verify(repository, never()).persist(any(CourseEntity.class));
   }
 
   @Test
   void should_delete_course() {
-    source.send(new CourseEvent(CourseEventType.COURSE_DELETED, LAAJIS));
+    service.delete(LAAJIS);
     verify(repository, atLeastOnce()).deleteById(111L);
   }
 

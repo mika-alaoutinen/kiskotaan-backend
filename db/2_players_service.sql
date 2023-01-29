dbe206102c80ba0308c10ed7f2e08688a00bf0b0
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-01-29 11:36:32 +0200

    Fix players init script

--- db/2_players_service.sql
+++ db/2_players_service.sql
@@ -7,17 +7,14 @@ CREATE DATABASE players_db WITH
 \c players_db;
 
 CREATE TABLE IF NOT EXISTS player (
-  id          SERIAL PRIMARY KEY,
-  first_name   varchar(50) NOT NULL,
-  last_name    varchar(50) NOT NULL
+  id            SERIAL PRIMARY KEY,
+  first_name    varchar(50) NOT NULL,
+  last_name     varchar(50) NOT NULL
 );
 
 -- Insert test data
-INSERT INTO player (id, first_name, last_name) VALUES
-  (1, 'Pekka', 'Kana'),
-  (2, 'Kalle', 'Kukko'),
-  (3, 'Aku', 'Ankka'),
-  (4, 'Iines', 'Ankka');
-
--- Hibernate sequence starts from 5 because there are 24 player entries
-CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START 5;
+INSERT INTO player (first_name, last_name) VALUES
+  ('Pekka', 'Kana'),
+  ('Kalle', 'Kukko'),
+  ('Aku', 'Ankka'),
+  ('Iines', 'Ankka');

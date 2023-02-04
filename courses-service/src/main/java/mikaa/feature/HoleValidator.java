e4e78307a357cdbf3516146a9e3712a4348ca6a6
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-02-04 19:12:26 +0200

    Add HoleValidationsTest

--- courses-service/src/main/java/mikaa/feature/HoleValidator.java
+++ courses-service/src/main/java/mikaa/feature/HoleValidator.java
@@ -12,7 +12,7 @@ interface HoleValidator {
         .anyMatch(num -> num == hole.getHoleNumber());
 
     if (duplicateHoleNumber) {
-      var error = new ValidationError("holeNumber", "Duplicate hole number");
+      var error = new ValidationError("number", "Duplicate hole number");
       ValidationException.maybeThrow(error);
     }
   }

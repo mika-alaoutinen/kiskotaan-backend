a61f3e6f65088e8a866452f27109e2f7002891c0
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-02-04 16:58:14 +0200

    Add tests

--- courses-service/src/test/java/mikaa/feature/CourseValidationsTest.java
+++ courses-service/src/test/java/mikaa/feature/CourseValidationsTest.java
@@ -9,10 +9,10 @@ import io.restassured.response.ValidatableResponse;
 import mikaa.dto.NewCourseDTO;
 import mikaa.dto.NewCourseNameDTO;
 import mikaa.dto.NewHoleDTO;
+import mikaa.errors.ValidationError;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -21,9 +21,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static io.restassured.RestAssured.given;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @QuarkusTest
 class CourseValidationsTest {
@@ -40,15 +38,21 @@ class CourseValidationsTest {
   void should_reject_new_course_without_holes() {
     var invalidCourse = new NewCourseDTO("New Course", List.of());
 
-    given()
-        .contentType(ContentType.JSON)
-        .body(invalidCourse)
-        .when()
-        .post(ENDPOINT)
-        .then()
-        .statusCode(400)
-        .contentType(ContentType.JSON);
+    var response = postInvalidCourse(invalidCourse);
+    assertBadRequest(response, new ValidationError("holes", "Course can haven 1-30 holes"));
 
+    verify(repository, never()).persist(any(CourseEntity.class));
+  }
+
+  @Test
+  void should_reject_new_course_with_duplicate_name() {
+    when(repository.existsByName(anyString())).thenReturn(true);
+    
+    var hole = new NewHoleDTO(1, 3, 100);
+    var invalidCourse = new NewCourseDTO("Duplicate name", List.of(hole));
+    var response = postInvalidCourse(invalidCourse);
+
+    assertBadRequest(response, new ValidationError("name", "Course name should be unique"));
     verify(repository, never()).persist(any(CourseEntity.class));
   }
 
@@ -78,6 +82,27 @@ class CourseValidationsTest {
         .contentType(ContentType.JSON);
 
     verify(holeRepository, never()).persist(any(HoleEntity.class));
+  }
+
+  private ValidatableResponse postInvalidCourse(NewCourseDTO invalidCourse) {
+    return given()
+        .contentType(ContentType.JSON)
+        .body(invalidCourse)
+        .when()
+        .post(ENDPOINT)
+        .then();
+  }
+
+  private static void assertBadRequest(ValidatableResponse res, ValidationError expected) {
+    res.statusCode(400)
+        .contentType(ContentType.JSON)
+        .body(
+            "timestamp", notNullValue(),
+            "status", is(400),
+            "error", is("Bad Request"),
+            "path", containsString("api/courses"),
+            "validationErrors[0].field", is(expected.field()),
+            "validationErrors[0].message", is(expected.message()));
   }
 
 }

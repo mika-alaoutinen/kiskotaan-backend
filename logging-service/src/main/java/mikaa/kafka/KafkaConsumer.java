4870388c91ac8e04ad7708f7613e066da9b9aac3
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-04-01 12:56:40 +0300

    Upgrade logging service to Quarkus 3

--- logging-service/src/main/java/mikaa/kafka/KafkaConsumer.java
+++ logging-service/src/main/java/mikaa/kafka/KafkaConsumer.java
@@ -1,6 +1,6 @@
 package mikaa.kafka;
 
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;

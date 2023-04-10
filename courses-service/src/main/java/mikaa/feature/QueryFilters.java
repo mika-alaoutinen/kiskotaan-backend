937444762223b09c9d0d348ec08bc8f0f12a2102
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-04-10 14:18:34 +0300

    Tests for QueryFilters

--- courses-service/src/main/java/mikaa/feature/QueryFilters.java
+++ courses-service/src/main/java/mikaa/feature/QueryFilters.java
@@ -1,12 +1,14 @@
 package mikaa.feature;
 
 import mikaa.util.StringFilter;
+import lombok.RequiredArgsConstructor;
 import mikaa.util.RangeFilter;
 
-record QueryFilters(
-    StringFilter nameFilter,
-    RangeFilter<Integer> holesFilter,
-    RangeFilter<Integer> parFilter) {
+@RequiredArgsConstructor
+class QueryFilters {
+  private final StringFilter nameFilter;
+  private final RangeFilter<Integer> holesFilter;
+  private final RangeFilter<Integer> parFilter;
 
   boolean applyAll(CourseEntity course) {
     var holes = course.getHoles();

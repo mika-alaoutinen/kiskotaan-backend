3aed5d84de8b066af5f3acb723df926f2bffa654
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-04-22 17:21:44 +0300

    Add separate topics for adding, updating and deleting holes

--- courses-service/src/test/java/mikaa/feature/HoleEventsTest.java
+++ courses-service/src/test/java/mikaa/feature/HoleEventsTest.java
@@ -12,11 +12,9 @@ import java.util.Optional;
 
 import jakarta.enterprise.inject.Any;
 import jakarta.inject.Inject;
-import mikaa.events.holes.HoleEvent;
 import mikaa.events.holes.HolePayload;
 import mikaa.events.holes.HoleProducer;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -44,62 +42,58 @@ class HoleEventsTest {
   @InjectMock
   private HoleRepository repository;
 
-  private InMemorySink<HoleEvent> sink;
   private HoleService service;
 
   @BeforeEach
   void setup() {
-    sink = connector.sink("holes-out");
-    sink.clear();
     service = new HoleService(courseService, producer, repository);
-  }
-
-  @AfterEach
-  void teardown() {
-    sink.clear();
   }
 
   @Test
   void should_send_event_on_add() {
+    InMemorySink<HolePayload> sink = connector.sink("hole-added");
+
     when(courseService.findOne(anyLong())).thenReturn(courseMock());
     var newHole = new HoleEntity(null, 1, 3, 100, courseMock());
     service.add(COURSE_ID, newHole);
 
     // holeId is null because mocked repository does not create a new ID on persist
-    var expected = new HolePayload(null, COURSE_ID, 1, 3, 100);
-    assertEvent("HOLE_ADDED", expected);
+    assertEvent(sink, new HolePayload(null, COURSE_ID, 1, 3, 100));
     verify(repository, atLeastOnce()).persist(any(HoleEntity.class));
   }
 
   @Test
   void should_send_event_on_update() {
+    InMemorySink<HolePayload> sink = connector.sink("hole-updated");
+
     when(repository.findByIdOptional(anyLong())).thenReturn(Optional.of(holeMock()));
     service.update(HOLE_ID, new HoleEntity(null, 4, 5, 165, courseMock()));
 
-    var expected = new HolePayload(HOLE_ID, COURSE_ID, 4, 5, 165);
-    assertEvent("HOLE_UPDATED", expected);
+    assertEvent(sink, new HolePayload(HOLE_ID, COURSE_ID, 4, 5, 165));
   }
 
   @Test
   void should_send_event_on_delete() {
+    InMemorySink<Long> sink = connector.sink("hole-deleted");
+
     when(repository.findByIdOptional(anyLong())).thenReturn(Optional.of(holeMock()));
     service.delete(HOLE_ID);
 
-    var expected = new HolePayload(HOLE_ID, COURSE_ID, 2, 3, 123);
-    assertEvent("HOLE_DELETED", expected);
+    assertEquals(1, sink.received().size());
+    var payload = sink.received().get(0).getPayload();
+    assertEquals(HOLE_ID, payload);
     verify(repository, atLeastOnce()).deleteById(anyLong());
   }
 
-  private void assertEvent(String eventName, HolePayload expected) {
+  private void assertEvent(InMemorySink<HolePayload> sink, HolePayload expected) {
     assertEquals(1, sink.received().size());
-    var event = sink.received().get(0).getPayload();
+    var payload = sink.received().get(0).getPayload();
 
-    assertEquals(eventName, event.type().toString());
-    assertEquals(expected.id(), event.payload().id());
-    assertEquals(expected.courseId(), event.payload().courseId());
-    assertEquals(expected.number(), event.payload().number());
-    assertEquals(expected.distance(), event.payload().distance());
-    assertEquals(expected.par(), event.payload().par());
+    assertEquals(expected.id(), payload.id());
+    assertEquals(expected.courseId(), payload.courseId());
+    assertEquals(expected.number(), payload.number());
+    assertEquals(expected.distance(), payload.distance());
+    assertEquals(expected.par(), payload.par());
   }
 
   private static CourseEntity courseMock() {

5c3b783ac5a1fda0ee455412e08a44886dc3afb3
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-04-28 15:29:45 +0300

    Add annotation

--- courses-service/src/main/java/mikaa/events/holes/HolePayload.java
+++ courses-service/src/main/java/mikaa/events/holes/HolePayload.java
@@ -1,4 +1,7 @@
 package mikaa.events.holes;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record HolePayload(Long id, Long courseId, int number, int par, int distance) {
 }

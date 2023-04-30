7eb31c5214ec3fe6b76403d26d0a7ca8bb4f617d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-04-30 13:10:27 +0300

    Fix SQL syntax errors

--- scorecards-service/src/main/resources/db/all/05_scorecard_player_table.sql
+++ scorecards-service/src/main/resources/db/all/05_scorecard_player_table.sql
@@ -1,5 +1,8 @@
 -- changeset mikaa:create_table_scorecard_player
 CREATE TABLE scorecard_player (
   player_id       bigint    NOT NULL,
-  scorecard_id    bigint    NOT NULL
+  scorecard_id    bigint    NOT NULL,
+
+  CONSTRAINT fk_player FOREIGN KEY(player_id) REFERENCES player(id),
+  CONSTRAINT fk_scorecard FOREIGN KEY(scorecard_id) REFERENCES scorecard(id)
 );

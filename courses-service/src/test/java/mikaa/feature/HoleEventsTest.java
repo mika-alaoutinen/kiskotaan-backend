4488995dd8717128f1f50b8ff7a1526385223e5f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-05-18 18:50:00 +0300

    Fix test, sort of

--- courses-service/src/test/java/mikaa/feature/HoleEventsTest.java
+++ courses-service/src/test/java/mikaa/feature/HoleEventsTest.java
@@ -55,11 +55,10 @@ class HoleEventsTest {
     var sink = initSink(OutgoingChannels.Hole.HOLE_ADDED);
 
     when(courseService.findOne(anyLong())).thenReturn(courseMock());
-    var newHole = new HoleEntity(null, 1, 3, 100, courseMock());
+    var newHole = new HoleEntity(321l, 1, 3, 100, courseMock());
     service.add(COURSE_ID, newHole);
 
-    // holeId is null because mocked repository does not create a new ID on persist
-    assertEvent(sink, new HolePayload(null, COURSE_ID, 1, 3, 100));
+    assertEvent(sink, new HolePayload(321l, COURSE_ID, 1, 3, 100));
     verify(repository, atLeastOnce()).persist(any(HoleEntity.class));
   }
 

87a6a042c44be4d1f4fbf22ba78667a1aef9540d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-05-20 10:04:02 +0300

    Fix player test data

--- scorecards-service/src/main/resources/db/dev/02_players.sql
+++ scorecards-service/src/main/resources/db/dev/02_players.sql
@@ -1,6 +1,5 @@
 -- changeset mikaa:add_players_dev
 INSERT INTO player (external_id, first_name, last_name) VALUES
-  (1, 'Pekka', 'Kana'),
-  (2, 'Kalle', 'Kukko'),
-  (3, 'Aku', 'Ankka'),
-  (4, 'Iines', 'Ankka');
+  (1, 'Aku', 'Ankka'),
+  (2, 'Iines', 'Ankka'),
+  (3, 'Hessu', 'Hopo');

55cfcd7e8ee3295af8a826f432ed548fb001935f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-07-01 15:42:51 +0300

    Move test to different file

--- courses-service/src/test/java/mikaa/it/HolesIT.java
+++ courses-service/src/test/java/mikaa/it/HolesIT.java
@@ -24,6 +24,24 @@ class HolesIT {
   private HoleProducer producer;
 
   @Test
+  void should_add_hole_for_a_course() {
+    given()
+        .contentType(ContentType.JSON)
+        .body(new HoleDTO().number(3).par(3).distance(90))
+        .when()
+        .post(ENDPOINT + "/3/holes")
+        .then()
+        .statusCode(200)
+        .contentType(ContentType.JSON)
+        .body(
+            "number", is(3),
+            "par", is(3),
+            "distance", is(90));
+
+    verify(producer, atLeastOnce()).holeAdded(any(HolePayload.class));
+  }
+
+  @Test
   void should_update_hole() {
     given()
         .contentType(ContentType.JSON)

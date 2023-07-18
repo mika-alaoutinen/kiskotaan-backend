fc3a6855d02e27cf6efde041fedb8d6a4c4ec057
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-07-18 19:08:36 +0300

    Add key to player records

--- players-service/src/main/java/mikaa/players/producers/KafkaProducer.java
+++ players-service/src/main/java/mikaa/players/producers/KafkaProducer.java
@@ -10,21 +10,21 @@ import mikaa.kiskotaan.domain.PlayerPayload;
 @RequiredArgsConstructor
 class KafkaProducer implements PlayerProducer {
 
-  private final KafkaTemplate<String, PlayerPayload> template;
+  private final KafkaTemplate<Long, PlayerPayload> template;
 
   @Override
   public void playerAdded(PlayerPayload payload) {
-    template.send(PlayerTopics.PLAYER_ADDED, payload);
+    template.send(PlayerTopics.PLAYER_ADDED, payload.getId(), payload);
   }
 
   @Override
   public void playerDeleted(PlayerPayload payload) {
-    template.send(PlayerTopics.PLAYER_DELETED, payload);
+    template.send(PlayerTopics.PLAYER_DELETED, payload.getId(), payload);
   }
 
   @Override
   public void playerUpdated(PlayerPayload payload) {
-    template.send(PlayerTopics.PLAYER_UPDATED, payload);
+    template.send(PlayerTopics.PLAYER_UPDATED, payload.getId(), payload);
   }
 
 }

b1c9c62ca291088550b5dc32d45774cf2bd9d51b
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-11-05 11:12:59 +0200

    Topology probably works

--- queries-service-streams/src/main/java/mikaa/streams/TopologyProducer.java
+++ queries-service-streams/src/main/java/mikaa/streams/TopologyProducer.java
@@ -9,32 +9,40 @@ import org.apache.kafka.streams.kstream.Produced;
 
 import io.apicurio.registry.serde.avro.AvroSerde;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mikaa.kiskotaan.domain.CourseEvent;
+import mikaa.kiskotaan.domain.CoursePayload;
 
 @ApplicationScoped
 @RequiredArgsConstructor
+@Slf4j
 class TopologyProducer {
 
   private static final Serde<Long> KEY_SERDE = Serdes.Long();
   private final KafkaStreamsConfig config;
 
+  @Produces
   Topology topology() {
     var builder = new StreamsBuilder();
 
     courseTopics(builder);
 
-    return builder.build();
+    var topology = builder.build();
+    log.info("topology {}", topology.describe());
+
+    return topology;
   }
 
   private void courseTopics(StreamsBuilder builder) {
     var inputTopic = config.inputTopics().courses();
     var outputTopic = config.stateStores().courses();
-    var valueSerde = new AvroSerde<CourseEvent>();
 
     builder
-        .stream(inputTopic, Consumed.with(KEY_SERDE, valueSerde))
-        .to(outputTopic, Produced.with(KEY_SERDE, valueSerde));
+        .stream(inputTopic, Consumed.with(KEY_SERDE, new AvroSerde<CourseEvent>()))
+        .mapValues(CourseEvent::getPayload)
+        .to(outputTopic, Produced.with(KEY_SERDE, new AvroSerde<CoursePayload>()));
   }
 
 }
---
+++ queries-service-streams/src/test/java/mikaa/infra/LivenessCheckTest.java
@@ -0,0 +1,22 @@
+package mikaa.infra;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class LivenessCheckTest {
+
+  @Test
+  void has_liveness_endpoint() {
+    given()
+        .when().get("/q/health/live")
+        .then()
+        .statusCode(200)
+        .body("status", is("UP"));
+  }
+
+}

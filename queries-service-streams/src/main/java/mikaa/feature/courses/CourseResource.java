fe8ae145dd51c46ad94e00dc946739faf71c4c47
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-11-23 09:57:52 +0200

    Apparently Smallrye GraphQL doesn't support Multi

--- queries-service-streams/src/main/java/mikaa/feature/courses/CourseResource.java
+++ queries-service-streams/src/main/java/mikaa/feature/courses/CourseResource.java
@@ -1,10 +1,12 @@
 package mikaa.feature.courses;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
 
-import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -12,12 +14,12 @@ import jakarta.enterprise.context.ApplicationScoped;
 @GraphQLApi
 public class CourseResource {
 
-  private static final Course COURSE = new Course(1, "Course name", 50);
+  private static final Course COURSE = new Course(1, "Laajis", 50);
 
   @Description("Find all courses")
   @Query
-  public Multi<Course> courses() {
-    return Multi.createFrom().item(COURSE);
+  public Uni<Collection<Course>> courses() {
+    return Uni.createFrom().item(List.of(COURSE));
   }
 
   @Description("Find a single course by ID")

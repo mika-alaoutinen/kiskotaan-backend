77a49e68add6e95d6eeb5fae04e9c7b3e70e75b2
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-11-24 10:33:35 +0200

    Add CourseStore

--- queries-service-streams/src/main/java/mikaa/feature/courses/CourseService.java
+++ queries-service-streams/src/main/java/mikaa/feature/courses/CourseService.java
@@ -2,33 +2,22 @@ package mikaa.feature.courses;
 
 import java.util.Collection;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.RequiredArgsConstructor;
-import mikaa.kiskotaan.domain.CoursePayload;
-import mikaa.streams.InteractiveQueries;
-import mikaa.streams.KafkaStreamsConfig;
 
 @ApplicationScoped
 @RequiredArgsConstructor
 class CourseService {
 
-  private final InteractiveQueries queries;
-  private final KafkaStreamsConfig config;
+  private final CourseStore store;
 
   Collection<Course> streamCourses() {
-    Stream<CoursePayload> courses = queries.streamAll(storeName());
-    return courses.map(Course::fromPayload).toList();
+    return store.streamAll().map(Course::fromPayload).toList();
   }
 
   Optional<Course> findCourse(long id) {
-    Optional<CoursePayload> maybeCourse = queries.findById(id, storeName());
-    return maybeCourse.map(Course::fromPayload);
-  }
-
-  private String storeName() {
-    return config.stateStores().courses();
+    return store.findById(id).map(Course::fromPayload);
   }
 
 }

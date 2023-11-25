9ce7930ee27296e4fdc765c1d8c20c8f6ef6ee49
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-11-25 14:49:27 +0200

    Change input serde type

--- queries-service-streams/src/main/java/mikaa/streams/CoursesTopology.java
+++ queries-service-streams/src/main/java/mikaa/streams/CoursesTopology.java
@@ -1,11 +1,19 @@
 package mikaa.streams;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.state.Stores;
 
+import io.apicurio.registry.serde.SerdeConfig;
+import io.apicurio.registry.serde.avro.AvroKafkaSerdeConfig;
+import io.apicurio.registry.serde.avro.AvroSerde;
 import io.quarkus.kafka.client.serialization.ObjectMapperSerde;
 import mikaa.kiskotaan.domain.CourseEvent;
 import mikaa.kiskotaan.domain.CoursePayload;
@@ -14,15 +22,32 @@ interface CoursesTopology {
 
   static void build(StreamsBuilder builder, KafkaStreamsConfig config) {
     var keySerde = Serdes.Long();
+
+    var inputSerde = inputSerde(CourseEvent.class);
     var inputTopic = config.inputTopics().courses();
+
+    var outputSerde = new ObjectMapperSerde<>(CoursePayload.class);
     var outputTopic = config.stateStores().courses();
 
     builder
-        .stream(inputTopic, Consumed.with(keySerde, new ObjectMapperSerde<>(CourseEvent.class)))
+        .stream(inputTopic, Consumed.with(keySerde, inputSerde))
         .mapValues(CourseEvent::getPayload)
         .toTable(Materialized.<Long, CoursePayload>as(Stores.persistentKeyValueStore(outputTopic))
             .withKeySerde(keySerde)
-            .withValueSerde(new ObjectMapperSerde<>(CoursePayload.class)));
+            .withValueSerde(outputSerde));
+  }
+
+  private static <T extends SpecificRecord> Serde<T> inputSerde(Class<T> type) {
+    var serde = new AvroSerde<T>();
+    serde.configure(serdeConfig(), false);
+    return serde;
+  }
+
+  private static Map<String, Object> serdeConfig() {
+    Map<String, Object> config = new HashMap<>();
+    config.put(AvroKafkaSerdeConfig.USE_SPECIFIC_AVRO_READER, true);
+    config.put(SerdeConfig.REGISTRY_URL, "http://localhost:64673/apis/registry/v2");
+    return config;
   }
 
 }

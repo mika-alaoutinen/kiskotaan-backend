c064007accc1b0c4a28cd2df987dd47ed5adfaa2
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2024-02-10 12:19:06 +0200

    Add par to score entry record

--- scorecards-service/src/main/java/mikaa/producers/KafkaProducer.java
+++ scorecards-service/src/main/java/mikaa/producers/KafkaProducer.java
@@ -18,6 +18,9 @@ import mikaa.kiskotaan.scorecard.ScoreEntry;
 import mikaa.logic.ScoreCardInput;
 import mikaa.logic.ScoreLogic;
 import mikaa.config.OutgoingChannels;
+import mikaa.feature.course.CourseEntity;
+import mikaa.feature.course.HoleEntity;
+import mikaa.feature.score.ScoreEntity;
 import mikaa.feature.scorecard.ScoreCardEntity;
 
 @ApplicationScoped
@@ -62,7 +65,7 @@ class KafkaProducer implements ScoreCardProducer {
 
     var scores = entity.getScores()
         .stream()
-        .map(score -> mapper.map(score, ScoreEntry.class))
+        .map(score -> mapScore(score, entity.getCourse()))
         .toList();
 
     return new ScoreCardPayload(
@@ -71,6 +74,19 @@ class KafkaProducer implements ScoreCardProducer {
         entity.getPlayerIds(),
         results,
         scores);
+  }
+
+  private ScoreEntry mapScore(ScoreEntity score, CourseEntity course) {
+    var entry = mapper.map(score, ScoreEntry.class);
+    int par = course.getHoles()
+        .stream()
+        .filter(hole -> hole.getNumber() == score.getHole())
+        .findFirst()
+        .map(HoleEntity::getPar)
+        .orElse(0);
+
+    entry.setPar(par);
+    return entry;
   }
 
 }

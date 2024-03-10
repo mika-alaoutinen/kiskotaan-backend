7338107523dba0e99167b87fc90c245c54f1761f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2024-03-10 17:42:39 +0200

    Delete unused records

--- scorecards-service/src/main/java/mikaa/logic/ScoresByHole.java
+++
@@ -1,9 +0,0 @@
-package mikaa.logic;
-
-import java.util.List;
-import java.util.Map;
-
-public record ScoresByHole(
-    Map<Long, PlayerScore> results,
-    Map<Integer, List<ScoreEntry>> scores) {
-}
--- scorecards-service/src/main/java/mikaa/logic/ScoresByPlayer.java
+++
@@ -1,9 +0,0 @@
-package mikaa.logic;
-
-import java.util.List;
-import java.util.Map;
-
-public record ScoresByPlayer(
-    Map<Long, PlayerScore> results,
-    Map<Long, List<ScoreEntry>> scores) {
-}
